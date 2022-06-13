b7880ea614fadc916460a130947348f8fe9f8267
ashish <ashirvadbhushan@gmail.com>
2022-06-13 09:31:42 +0530

    prettier

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
+import * as React from "react";
 
 export default function Index() {
     return (
         <React.Fragment>
             <div className="bg-zinc-800 text-white flex justify-center items-center h-screen w-screen flex-col">
-                <h2 className='text-3xl font-extrabold'>404</h2>
+                <h2 className="text-3xl font-extrabold">404</h2>
                 <p>Page not found!</p>
             </div>
         </React.Fragment>
-    )
+    );
 }
